bd878e80a47b68bf1056e46d5755396bf3976ae9
akgmf9 <agokgmf@gmail.com>
2025-03-30 00:35:31 +0100

    GUI Kartenmanager_SuS.java
    GUI-Elemente implementiert

--- Kartenmanager_SuS.java
+++ Kartenmanager_SuS.java
@@ -1,219 +1,122 @@
 import sum.ereignis.*;
-
 import sum.komponenten.*;
-
 import sum.werkzeuge.*;
-
 import sum.strukturen.*;
 
-
-
 /**
-
  * Kartenmanager_SuS:
-
  * Verwendung einer Liste<Karte> (aus sum.strukturen.Liste) 
-
  * anstelle eines Arrays. 
-
  * Die GUI wurde beibehalten (Stapelgr., Wert, Farbe, Position usw.).
-
  * - Einfügen/Entfernen mit Liste
-
  * - Sortieren/Erneuern (Platzhalter) 
-
  * - Zeichnen ab y=150
-
  */
 
 public class Kartenmanager_SuS extends Ereignisanwendung {
-
-
-
     // -----------------------------
-
     // 1) GUI-Objekte
-
     // -----------------------------
-
     private Knopf startKnopf;   // "Sortieren"
-
     private Knopf updateKnopf;  // "Erneuern"
-
     private Knopf einfKnopf;    // "Einfügen"
-
     private Knopf entfKnopf;    // "Entfernen"
 
-
-
     private Etikett infoEtikett;    
-
     private Etikett labelStapelsize; 
-
     private Etikett labelWert;  
-
     private Etikett labelFarbe; 
-
     private Etikett labelPos;   
 
-
-
     private Auswahl auswahlWert;   
-
     private Auswahl auswahlFarbe;  
 
-
-
     private Textfeld tfUmfang;  // neue Stapelgröße
-
     private Textfeld tfPos;     // Position für Einfügen/Entfernen
-
-
 
     private Buntstift stift;
 
-
-
     // -----------------------------
-
     // 2) Listen-Datenstruktur
-
     // -----------------------------
-
     // Karte => Hilfsklasse für (wert, farbe)
-
     private class Karte {
-
         int wert;   // 1..13 => "2..Ass"
-
         int farbe;  // 0..3  => "Kreuz..Pik"
-
-
-
         public Karte(int w, int f) {
-
             this.wert = w;
-
             this.farbe= f;
-
         }
-
     }
 
-
-
     // Unsere Liste<Karte> anstelle von int[][]
-
     private Liste<Karte> karten;
-
     private int kartenAnzahl = 52;  // "logische" Stapelgröße
 
-
-
     // Namen für Werte und Farben (Index 1..13 => "2..Ass")
-
     private String[] werteNamen = {
-
             "", "2", "3", "4", "5", "6", 
-
             "7", "8", "9", "10", "Bube", "Dame", "König", "Ass"
-
         };
 
     private String[] farbenNamen = {
-
             "Kreuz", "Karo", "Herz", "Pik"
-
         };
 
-
-
     // Zeitmessung
-
     private long startZeit;
-
     private long endZeit;
 
-
-
     public Kartenmanager_SuS() {
-
         super();
-
-
-
         stift = new Buntstift();
 
-
-
         // 3) GUI: 
-
         //    - InfoEtikett + Stapelgröße/Erneuern/Sortieren (links)
-
         //    - Wert/Farbe/Position + Einfügen/Entfernen (rechts)
-
         //    - Karten ab y=150
 
 
 
         infoEtikett    = new Etikett( 50,  20, 400, 30, 
-
             "Karten mit Liste (Einfügen/Entfernen).");
 
-
-
         labelStapelsize= new Etikett( 50,  60, 80, 25, "Stapelgr.:");
-
         tfUmfang       = new Textfeld(130, 60, 60, 25, "52"); // Standard=52
-
         updateKnopf    = new Knopf(200, 60, 100, 30, "Erneuern");
-
         startKnopf     = new Knopf(310, 60, 100, 30, "Sortieren");
+        tfDurchgänge   = new Textfeld(310, 100, 100, 30, "1000");
+        auswahlSort    = new Auswahl(310, 20, 100, 30);
+
+        auswahlSort.haengeAn("Bubblesort");
+        auswahlSort.haengeAn("Instertsort");
+        auswahlSort.haengeAn("Mergesort");
 
 
 
         // Wert/Farbe/Position 
 
         labelWert   = new Etikett(450,  20, 40, 25, "Wert:");
-
         auswahlWert = new Auswahl(500, 20, 80, 25);
 
-
-
         labelFarbe  = new Etikett(600, 20, 50, 25, "Farbe:");
-
         auswahlFarbe= new Auswahl(660,20, 80, 25);
 
-
-
         labelPos    = new Etikett(450, 60, 60, 25, "Position:");
-
         tfPos       = new Textfeld(520, 60, 60, 25, "0");
 
-
-
         einfKnopf   = new Knopf(600, 60, 100, 30, "Einfügen");
-
         entfKnopf   = new Knopf(710, 60, 100, 30, "Entfernen");
 
-
-
         // Auswahllisten befüllen: Index 0..12 => "2..Ass"
-
         for(int w=1; w<=13; w++){
-
             auswahlWert.haengeAn(werteNamen[w]);
-
         }
 
         // Index 0..3 => "Kreuz..Pik"
-
         for(int f=0; f<4; f++){
-
             auswahlFarbe.haengeAn(farbenNamen[f]);
-
         }
-
 
 
         // 4) Ereignismethoden
